e3450470983fef6ebb84dd7930ab4a658835ddc4
linhaoran <1476589247@qq.com>
2019-02-27 18:12:33 +0800

    flutterSwiper和屏幕适配

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_shop/helper/http.dart' show RestApi;
+import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,93 +10,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = '你选择的类型';
+  
+  @override
+  Widget build(BuildContext context) {
+    // ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('百姓生活+'),),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data);
+            List<Map> swiper = (data['data']['slides'] as List).cast();
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDateList: swiper)
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中.....'),
+            );
+          }
+        },
+      )
+    );
+  }
+}
+
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+
+  final List swiperDateList;
+  SwiperDiy({Key key, this.swiperDateList}):super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+    print('设备像素密度:${ScreenUtil.pixelRatio}');
+    print('设备高:${ScreenUtil.screenHeight}');
+    print('设备高DP:${ScreenUtil.screenHeightDp}');
+    print('设备宽:${ScreenUtil.screenWidth}');
+    print('设备宽DP:${ScreenUtil.screenWidthDp}');
     return Container(
-      child: Scaffold(
-        appBar: AppBar(title: Text('Get请求'),),
-        body: SingleChildScrollView(
-          child: Container(
-            height: 1000,
-            child: Column(
-              children: <Widget>[
-                TextField(
-                  controller: typeController,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(10.0),
-                    labelText: '类型',
-                    helperText: '请输入你的类型'
-                  ),
-                  autofocus: false,
-                ),
-                RaisedButton(
-                  onPressed: _choiceAction,
-                  child: Text('选择完毕'),
-                ),
-                Text(
-                  showText,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                )
-              ],
-            ),
-          ),
-        ),
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
+      child: Swiper(
+        itemCount: swiperDateList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network('${swiperDateList[index]['image']}');
+        },
+        pagination: SwiperPagination(),
+        autoplay: true,
       ),
     );
-  }
-  void _choiceAction () {
-    print('开始选择你的类型....');
-    if(typeController.text.toString() == '') {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(title: Text('类型不能为空'),)
-      );
-    } else {
-      // getHttp(typeController.text.toString()).then((res) {
-      //   setState(() {
-      //     print('测试$res');
-      //     showText = res['data']['name'].toString();
-      //   });
-      // });
-
-      // 调用封装get
-      // RestApi.get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian', {'name': typeController.text.toString()})
-      // .then((res) {
-      //     print('测试1$res');
-      //     // showText = res['data']['name'].toString();
-      //   }
-      // );
-
-      // 调用封装post
-      RestApi.post('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/post_dabaojian', {'name': typeController.text.toString()})
-      .then((res) {
-        setState(() {
-          print('测试2$res');
-          showText = res['data']['name'].toString();
-        });
-      });
-    }
-  }
-
-  Future getHttp(String TypeText) async {
-    try{
-      Response response;
-      var data = {'name': TypeText};
-      // response = await Dio().get(
-      //   'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian',
-      //   queryParameters: data
-      // );
-      response = await Dio().post(
-        'https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/post_dabaojian',
-        queryParameters: data
-      );
-      return response.data;
-    } catch(e) {
-      return print(e);
-    }
   }
 }

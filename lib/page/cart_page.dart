5be1f7c5aab1d8e728657eaa4f548f4146fd0e52
linhaoran <1476589247@qq.com>
2019-03-26 22:47:44 +0800

    24.Provide状态管理基础

--- lib/page/cart_page.dart
+++ lib/page/cart_page.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
-import '../service/service_method.dart';
+import 'package:provide/provide.dart';
+import '../provide/counter.dart';
 
-class CartPage extends StatefulWidget {
-  @override
-  _CartPageState createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
-  String homePageContent = '正在获取数据';
-  @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      getHomePageContent().then((val) {
-        print('购物车数据：$val');
-        setState(() {
-          homePageContent = val.toString();
-        });
-      });
-    }
+class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('百姓生活+'),),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+      appBar: AppBar(title: Text('购物车页面'),),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Number(),
+            MyButton()
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class Number extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 200),
+      child: Provide<Counter>(
+        builder: (context, child, counter) {
+          return Text(
+            '${counter.value}',
+            style: Theme.of(context).textTheme.display1,
+          );
+        },
+      )
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: (){
+        Provide.value<Counter>(context).increment();
+      },
+      child: Text('增加'),
     );
   }
 }

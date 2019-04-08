8562ae7c9f7560e8e419b4becc50d4f59ccfe682
sam <sam@haorandeMacBook-Pro.local>
2019-04-08 21:28:19 +0800

    feat: 36.FlutterToast插件使用

--- lib/page/category_page.dart
+++ lib/page/category_page.dart
@@ -8,6 +8,7 @@ import '../provide/child_category.dart';
 import '../model/categoryGoodsList.dart';
 import '../provide/category_goods_list.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 
 class CateGoryPage extends StatefulWidget {
@@ -284,6 +285,14 @@ class _CategoryGoodsListState extends State<CategoryGoodsList> {
       var data = json.decode(val.toString());
       CategroyGoodsListModel goodsList = CategroyGoodsListModel.fromJson(data);
       if(goodsList.data == null) {
+        Fluttertoast.showToast(
+          msg: "已经到底了",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          backgroundColor: Colors.pink,
+          textColor: Colors.white,
+          fontSize: 16.0
+        );
         Provide.value<ChildCategory>(context).changeNoMore('没有更多数据了');
       } else {
         Provide.value<CategoryGoodsListProvide>(context).getMoreList(goodsList.data);

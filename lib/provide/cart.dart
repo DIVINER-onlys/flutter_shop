0523211e0e77f459774bf21f2aeb1c16c1e2d8e9
linhaoran <1476589247@qq.com>
2019-04-19 19:53:14 +0800

    feat:53.购物车_商品列表UI框架布局

--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../model/cartInfo.dart';
 
 class CartProvide with ChangeNotifier {
   String cartString = "[]";
+  List<CartInfoModel> cartList = [];
 
   save(goodsId, goodsName, count, price, images) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -15,23 +17,27 @@ class CartProvide with ChangeNotifier {
     tempList.forEach((item){
       if(item['goodsId'] == goodsId) {
         tempList[ival]['count'] = item['count']+1;
-        print(tempList[ival]['count']);
+        cartList[ival].count++;
         isHave = true;
       }
       ival++;
     });
     if(!isHave){
-      tempList.add({
+      Map<String, dynamic> newGoods = {
         'goodsId': goodsId,
         'goodsName': goodsName,
         'count': count,
         'price': price,
         'images': images
-      });
+      };
+
+      tempList.add(newGoods);
+      cartList.add(CartInfoModel.fromJson(newGoods));
     }
 
     cartString = json.encode(tempList).toString();
     print('购物车数据$cartString');
+    print('购物车数据数据模型$cartList');
     prefs.setString('cartInfo', cartString);
 
     notifyListeners();
@@ -40,7 +46,23 @@ class CartProvide with ChangeNotifier {
   remove() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('cartInfo');
+    cartList = [];
     print('清空购物车数据完成.........');
+    notifyListeners();
+  }
+
+  getCartInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    cartList = [];
+    if(cartString == null) {
+      cartList = [];
+    } else {
+      List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+      tempList.forEach((item){
+        cartList.add(CartInfoModel.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 

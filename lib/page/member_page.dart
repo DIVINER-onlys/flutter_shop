13f347ef811d2c1aacf7cdbfc2f02a89e0922fb7
linhaoran <1476589247@qq.com>
2019-04-21 00:31:04 +0800

    feat: 64.会员中心_顶部头像UI布局

--- lib/page/member_page.dart
+++ lib/page/member_page.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provide/provide.dart';
-import '../provide/counter.dart';
+
 class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Provide<Counter>(
-          builder: (context, child, counter) {
-            return Text('${counter.value}');
-          },
-        ),  
+      appBar: AppBar(
+        title: Text('会员中心'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          _topHeader()
+        ],
+      ),
+    );
+  }
+
+  Widget _topHeader(){
+    return Container(
+      width: ScreenUtil().setWidth(750),
+      padding: EdgeInsets.all(20),
+      color: Colors.pinkAccent,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            width: ScreenUtil().setWidth(120),
+            // height: ScreenUtil().setHeight(100),
+            child: ClipOval(
+              child: Image.network('http://www.gx8899.com/uploads/allimg/2018021008/mcjasrwyb13.jpg'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              'Sam',
+              style: TextStyle(fontSize: ScreenUtil().setSp(36), color: Colors.black54)
+            ),
+          )
+        ],
       ),
     );
   }
